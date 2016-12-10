99eb85edccaa62a2384039ddafd50fc0ebe482ec
Little John <feldstein@contactify.ch>
2016-12-10 18:26:41 +0100

    login for companys with fake call to mochdata

---
+++ login/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, session
+from base64 import base64encode
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+def valid_login(username,password):
+	if(username=="a" and password=="b"):
+        session["token"] = username;
+		return True
+
+@app.route('/login', methods=['GET','POST'])
+def authenticate():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+#            return log_the_user_in(request.form['username'])
+            return "Hello World"
+        else:
+            error = 'Invalid username/password'
+            return error 
+    else:
+        return "Wrong request"
+
+
+
+if __name__ == "__main__":
+	app.run()
